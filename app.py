327d39c962713c2df7de41ee7464a5c355d3d2fd
Victoria.Karpeeva <Victoria.Karpeeva@acronis.com>
2020-03-07 19:23:41 +0300

    Add basic logger and some refactoring

--- app.py
+++ app.py
@@ -1,16 +1,18 @@
-from pymongo import MongoClient
+import logging
 
 from core.words import Word
-from core.utils import add_new_word, get_words_list
-from const import GENDERS, PARTS_OF_SPEECH, FORMS
+from core.utils import save_word, get_words_list, delete_word
+from core.const import GENDERS, PARTS_OF_SPEECH, FORMS
 from flask import Flask, render_template, request
 
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
 @app.route('/', methods=['POST', 'GET'])
 @app.route('/index', methods=['POST', 'GET'])
-@app.route('/add', methods=['POST', 'GET'])
 def index():
     return render_template('index.html')
 
@@ -19,21 +21,17 @@ def index():
 def add():
     result = None
     if request.method == 'POST':
-        rus = request.form['inputRus']
-        ch = request.form['inputCz']
-        word_type = request.form['selectPartOfSpeech']
-        gender = request.form['selectGender']
-        form = request.form['selectForm']
-        word = Word(rus, ch, word_type, gender, form)
+        word = Word(request.form)
         try:
             if 'saveButton' in request.form:
-                add_new_word(word)
+                save_word(word)
                 result = 'Слово успешно сохранено!'
             if 'deleteButton' in request.form:
                 delete_word(word)
                 result = 'Слово успешно удалено!'
-        except:
+        except Exception as e:
             result = 'Ошибка!'
+            logger.error(str(e))
     words_list = get_words_list()
     return render_template('add.html', result=result, words_list=words_list,
                            genders=GENDERS, parts_of_speech=PARTS_OF_SPEECH, forms=FORMS)
@@ -45,5 +43,6 @@ def page_not_found(error):
 
 
 if __name__ == '__main__':
+    logger.info('*'*80)
     # app.run(host='0.0.0.0', port=4000, passthrough_errors=True, debug=False)  # for public
     app.run(port=4000, passthrough_errors=False, debug=True)  # for debug
--- core/words.py
+++ core/words.py
@@ -1,18 +1,18 @@
 class Word:
     def __init__(self, dictionary):
         self.rus = dictionary['rus']
-        self.ch = dictionary['ch']
+        self.cz = dictionary['cz']
         self.part_of_speech = dictionary['part_of_speech']
-        self.gender = dictionary['gender']
-        self.form = dictionary['form']
+        self.gender = dictionary['gender'] if self.part_of_speech == 'noun' else None
+        self.form = dictionary['form'] if self.part_of_speech == 'noun' else None
 
     def __str__(self):
-        return f'{self.rus}, {self.ch}, {self.part_of_speech}, {self.gender}, {self.form}'
+        return f'{self.rus}, {self.cz}, {self.part_of_speech}, {self.gender}, {self.form}'
 
     def get_dict(self):
         return {
             'rus': self.rus,
-            'ch': self.ch,
+            'cz': self.cz,
             'part_of_speech': self.part_of_speech,
             'gender': self.gender,
             'form': self.form
