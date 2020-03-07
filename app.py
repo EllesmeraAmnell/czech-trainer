82287da7dd68e262f0f312cce12768532a133c38
Victoria.Karpeeva <Victoria.Karpeeva@acronis.com>
2020-03-07 21:34:12 +0300

    Add navigation bar and quiz page

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import logging
+from random import Random
 
-from core.words import Word
-from core.utils import save_word, get_words_list, delete_word
+from core.word import Word
+from core.utils import save_word, get_words_list, delete_word, get_random_words
 from core.const import GENDERS, PARTS_OF_SPEECH, FORMS
 from flask import Flask, render_template, request
 
@@ -17,8 +18,17 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/add', methods=['POST', 'GET'])
-def add():
+@app.route('/quiz', methods=['POST', 'GET'])
+def quiz():
+    words = get_random_words(4)
+    random_option = Random().choice(words)
+    question, answer = random_option.rus, random_option.cz
+    options = [word.cz for word in words]
+    return render_template('quiz.html', question=question, answer=answer, options=options)
+
+
+@app.route('/edit_db', methods=['POST', 'GET'])
+def edit_db():
     result = None
     if request.method == 'POST':
         word = Word(request.form)
@@ -33,7 +43,7 @@ def add():
             result = 'Ошибка!'
             logger.error(str(e))
     words_list = get_words_list()
-    return render_template('add.html', result=result, words_list=words_list,
+    return render_template('edit_db.html', result=result, words_list=words_list,
                            genders=GENDERS, parts_of_speech=PARTS_OF_SPEECH, forms=FORMS)
 
 
@@ -43,6 +53,6 @@ def page_not_found(error):
 
 
 if __name__ == '__main__':
-    logger.info('*'*80)
+    logger.info('*' * 80)
     # app.run(host='0.0.0.0', port=4000, passthrough_errors=True, debug=False)  # for public
     app.run(port=4000, passthrough_errors=False, debug=True)  # for debug
--- core/utils.py
+++ core/utils.py
@@ -1,6 +1,6 @@
 import logging
 
-from core.words import Word
+from core.word import Word
 from pymongo import MongoClient
 from random import Random
 
@@ -34,20 +34,20 @@ def get_words_list(limit=None, skip=None):
     return list(res) if res else None
 
 
-def get_options_list(count):
+def get_random_words(count):
     n = collection.count_documents({})
     if n < count:
         logger.error(f'Not enough words in database: {n}. Requested: {count}.')
         raise Exception(f'ERROR: Not enough words in database: {n}. Requested: {count}.')
     used_indexes = []
-    options_list = []
+    random_words = []
     random = Random()
-    for i in range(0, count):
+    for _ in range(count):
         new_index = random.randint(0, n - 1)
         while new_index in used_indexes:
             new_index = random.randint(0, n - 1)
         used_indexes.append(new_index)
         words_list = get_words_list(limit=1, skip=new_index)
         word = Word(words_list[0])
-        options_list.append(word)
-    return options_list
+        random_words.append(word)
+    return random_words
